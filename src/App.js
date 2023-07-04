cc8c3eb67e0e2989d8385ffbea074c9a680f7283
paul <paul.wyatt378@outlook.com>
2023-07-04 12:39:48 +0100

    new test app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My Awesome app</h1>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
